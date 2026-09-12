81173eed966cf120bccd99b342ab2aa0daaf338f
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-12 17:16:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ codes.forEach((code, index) => {
 
     code.addEventListener("keydown", (e) => {
 
-        if(e.key === "Backspace" && code.value === ""){
+        if(e.key === "Backspace" ){
 
+			code.value = "";
             if(index > 0){
                 codes[index - 1].focus();
             }
